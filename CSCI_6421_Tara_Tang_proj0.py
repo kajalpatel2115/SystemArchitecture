3bf3bd39cacb9383390caf2c40b914d529bb1daf
TaraTang925 <159092482+TaraTang925@users.noreply.github.com>
2024-02-07 18:51:07 -0500

    Add code comments in python script.
    Add explanations of functions.

--- CSCI_6421_Tara_Tang_proj0.py
+++ CSCI_6421_Tara_Tang_proj0.py
@@ -1,3 +1,4 @@
+# Author: Yue Tang
 import fileinput
 index_register = [0,0,0,0]
 
@@ -9,8 +10,10 @@ def binary2octal(input_binary, length):
     decimal_val = int(input_binary, 2)
     oct_val = oct(decimal_val)[2:]
     if (length-len(oct_val)) >= 0:
+        # make return string in the same length
         offsetZERO = (length-len(oct_val))
     else:
+        #check the range of value
         print("Invalid input: the value is bigger than maximum.")
         return
     return '0' * offsetZERO + oct_val
@@ -19,6 +22,7 @@ def decimal2binary(x, length):
     # x is a int value
     val = bin(x)[2:]
     if (length-len(val)) >= 0:
+        # make return string in the same length
         offsetZERO = (length-len(val))
     else:
         print("Invalid input: the value is bigger than maximum.")
@@ -29,6 +33,7 @@ def decimal2octal(x, length):
     # x is a int value
     val = oct(x)[2:]
     if (length-len(val)) >= 0:
+        # make return string in the same length
         offsetZERO = (length-len(val))
     else:
         print("Invalid input: the value is bigger than maximum.")
@@ -36,6 +41,7 @@ def decimal2octal(x, length):
     return '0' * offsetZERO + val
 
 def process_binstr_register(opcode, content_list):
+    # the most translator function, deal with most of instructions
     bin_opcode = decimal2binary(opcode, 6)
     decimal_R = int(content_list[0])
     decimal_IX = int(content_list[1])
@@ -49,15 +55,17 @@ def process_binstr_register(opcode, content_list):
     return bin_str
 
 def process_binstr_logic(opcode, content_list):
+    # translate logic instructions, including MLT, DVD, TRR, AND, ORR, NOT 
     bin_opcode = decimal2binary(opcode, 6)
     decimal_Rx = int(content_list[0])
     decimal_Ry = int(content_list[1])
     bin_Rx = decimal2binary(decimal_Rx, 2)
     bin_Ry = decimal2binary(decimal_Ry, 2)
-    bin_str = bin_opcode + bin_Rx + bin_Ry + "000000"
+    bin_str = bin_opcode + bin_Rx + bin_Ry + "000000" # address part is ignored
     return bin_str
 
 def process_binstr_count(opcode, content_list):
+    # translate logic instructions, including SRC, RRC 
     bin_opcode = decimal2binary(opcode, 6)
     decimal_R = int(content_list[0])
     decimal_count = int(content_list[1])
@@ -67,239 +75,303 @@ def process_binstr_count(opcode, content_list):
     bin_AL = decimal2binary(decimal_AL, 1)
     bin_LR = decimal2binary(decimal_LR, 1)
     bin_count = decimal2binary(decimal_count, 4)
-    bin_str = bin_opcode + bin_R + bin_AL + bin_LR + "00" + bin_count
+    bin_str = bin_opcode + bin_R + bin_AL + bin_LR + "00" + bin_count # "00" part is ignored
     return bin_str
 
 def process_binstr_IO(opcode, content_list):
+    # translate logic instructions, including IN, OUT, CHK 
     bin_opcode = decimal2binary(opcode, 6)
     decimal_R = int(content_list[0])
     decimal_devid = int(content_list[1])
     bin_R = decimal2binary(decimal_R, 2)
     bin_devid = decimal2binary(decimal_devid, 5)
-    bin_str = bin_opcode + bin_R + "000" + bin_devid
+    bin_str = bin_opcode + bin_R + "000" + bin_devid # "00" part is ignored
     return bin_str
 
 def process_str(input_str):
-    opcode = 0
+    # process different opcode
+    opcode = 0 # it's a decimal value
     if input_str == 'HLT':
-        print('HLT')
         return 'HLT'
     if (len(input_str.split()) != 2):
         print("Invalid input string.")
         return
+    #op: get type of instructions
     op = input_str.split()[0]
+    #content: get the param string
     content = input_str.split()[1]
+    #con_value: split the param string into list
+    #the datatype of ele in this list is string (convert to int in orther function)
     con_value = content.split(',')
-    print("content: " , content)
-    print("con_value: ", con_value, type(con_value))
     if op == "LOC":
+        # load location
         opcode = 0
         return "LOC", int(con_value[0])
     elif op == "Data":
+        # load data value
         opcode = 0
         if con_value[0] == 'End':
             con_value[0] = 1024
         return "Data", decimal2octal(int(con_value[0]), 6)
     elif op == "LDR":
+        # input: LDR r, x, address[,I]
         opcode = 1
         if len(con_value) == 3:
+            # if there isn't I value, append zero
             con_value.append(0)
         bin_str = process_binstr_register(opcode, con_value)
         return "LDR", binary2octal(bin_str, 6)
     elif op == "STR":
+        # input: STR r, x, address[,I]
         opcode = 2
         if len(con_value) == 3:
+            # if there isn't I value, append zero
             con_value.append(0)
         bin_str = process_binstr_register(opcode, con_value)
         return "STR", binary2octal(bin_str, 6)
     elif op == "LDA":
+        # input: LDA r, x, address[,I]
         opcode = 3
         if len(con_value) == 3:
+            # if there isn't I value, append zero
             con_value.append(0)
         bin_str = process_binstr_register(opcode, con_value)
         return "LDA", binary2octal(bin_str, 6)
     elif op == "LDX":
+        # input: LDX x, address[,I]
         opcode = 4
         if len(con_value) == 2:
+            # if there isn't I value, append zero
             con_value.append(0)
-        con_value = [0] + con_value
+        con_value = [0] + con_value # add R in the beginning of params
         bin_str = process_binstr_register(opcode, con_value)
         return "LDX", binary2octal(bin_str, 6)
     elif op == "STX":
+        # input: STX x, address[,I]
         opcode = 5
         if len(con_value) == 2:
+            # if there isn't I value, append zero
             con_value.append(0)
-        con_value = [0] + con_value
+        con_value = [0] + con_value # add R in the beginning of params
         bin_str = process_binstr_register(opcode, con_value)
         return "STX", binary2octal(bin_str, 6)
-    # the code below is not available now, only draft.
     elif op == "JZ":
+        # input: JZ x, address[,I]
         opcode = 6
         if len(con_value) == 2:
+            # if there isn't I value, append zero
             con_value.append(0)
-        con_value = [0] + con_value
+        con_value = [0] + con_value # add R in the beginning of params
         bin_str = process_binstr_register(opcode, con_value)
         return "JZ", binary2octal(bin_str, 6)
     elif op == "JNE":
+        # input: JNE x, address[,I]
         opcode = 7
         if len(con_value) == 2:
+            # if there isn't I value, append zero
             con_value.append(0)
         con_value = [0] + con_value
         bin_str = process_binstr_register(opcode, con_value)
         return "JNE", binary2octal(bin_str, 6)
-    elif op == "JCC": # todo
-        opcode = 8
+    elif op == "JCC": 
+        # input: JCC cc, x, address[,I
+        opcode = 8 # it's a decimal number (10 as a octal number)
         if len(con_value) == 3:
+            # if there isn't I value, append zero
             con_value.append(0)
         bin_str = process_binstr_register(opcode, con_value)
         return "JCC", binary2octal(bin_str, 6)
     elif op == "JMA":
-        opcode = 9
+        # input: JMA x, address[,I]
+        opcode = 9 # it's a decimal number (11 as a octal number)
         if len(con_value) == 2:
+            # if there isn't I value, append zero
             con_value.append(0)
         con_value = [0] + con_value
         bin_str = process_binstr_register(opcode, con_value)
         return "JMA", binary2octal(bin_str, 6)
-    elif op == "JSR": #todo
+    elif op == "JSR": 
+        # input: JSR x, address[,I]
         opcode = 10
         if len(con_value) == 2:
+            # if there isn't I value, append zero
             con_value.append(0)
         con_value = [0] + con_value
         bin_str = process_binstr_register(opcode, con_value)
         return "JSR", binary2octal(bin_str, 6)
-    elif op == "RFS": #todo
+    elif op == "RFS":
+        # input: RFS Immed
         opcode = 11
         con_value = [0, 0] + con_value + [0]
         bin_str = process_binstr_register(opcode, con_value)
         return "RFS", binary2octal(bin_str, 6)
     elif op == "SOB":
+        # input: SOB r, x, address[,I]
         opcode = 12
         if len(con_value) == 3:
+            # if there isn't I value, append zero
             con_value.append(0)
         bin_str = process_binstr_register(opcode, con_value)
         return "SOB", binary2octal(bin_str, 6)
     elif op == "JGE":
+        # input: JGE r,x, address[,I]
         opcode = 13
         if len(con_value) == 3:
+            # if there isn't I value, append zero
             con_value.append(0)
         bin_str = process_binstr_register(opcode, con_value)
         return "JGE", binary2octal(bin_str, 6)
     elif op == "AMR":
+        # input: AMR r, x, address[,I]
         opcode = 14
         if len(con_value) == 3:
+            # if there isn't I value, append zero
             con_value.append(0)
         bin_str = process_binstr_register(opcode, con_value)
         return "AMR", binary2octal(bin_str, 6)
     elif op == "SMR":
+        # input: SMR r, x, address[,I]
         opcode = 15
         if len(con_value) == 3:
+            # if there isn't I value, append zero
             con_value.append(0)
         bin_str = process_binstr_register(opcode, con_value)
         return "SMR", binary2octal(bin_str, 6)
     elif op == "AIR": # TODO
+        # input: AIR r, immed
         opcode = 16
         con_value = con_value[0] + [0] + con_value[1] + [0]
+        # offset the ignored params
         bin_str = process_binstr_register(opcode, con_value)
         return "AIR", binary2octal(bin_str, 6)
     elif op == "SIR":
+        # input: SIR r, immed
         opcode = 17
         con_value = con_value[0] + [0] + con_value[1] + [0]
+        # offset the ignored params
         bin_str = process_binstr_register(opcode, con_value)
         return "SIR", binary2octal(bin_str, 6)
     elif op == "MLT":
+        # input: MLT rx,ry
         opcode = 18
         bin_str = process_binstr_logic(opcode, con_value)
         return "MLT", binary2octal(bin_str, 6)
     elif op == "DVD":
+        # input: DVD rx,ry
         opcode = 19
         bin_str = process_binstr_logic(opcode, con_value)
         return "DVD", binary2octal(bin_str, 6)
     elif op == "TRR":
+        # input: TRR rx,ry
         opcode = 20
         bin_str = process_binstr_logic(opcode, con_value)
         return "TRR", binary2octal(bin_str, 6)
     elif op == "AND":
+        # input: AND rx,ry
         opcode = 21
         bin_str = process_binstr_logic(opcode, con_value)
         return "AND", binary2octal(bin_str, 6)
     elif op == "ORR":
+        # input: ORR rx,ry
         opcode = 22
         bin_str = process_binstr_logic(opcode, con_value)
         return "ORR", binary2octal(bin_str, 6)
     elif op == "NOT":
+        # input: NOT rx
         opcode = 23
-        bin_str = process_binstr_logic(opcode, con_value + [0])
+        bin_str = process_binstr_logic(opcode, con_value + [0]) # offset the ignored params
         return "NOT", binary2octal(bin_str, 6)
     elif op == "SRC":
+        # input: SRC r, count, L/R, A/L
         opcode = 24
         bin_str = process_binstr_count(opcode, con_value)
         return "SRC", binary2octal(bin_str, 6)
     elif op == "RRC":
+        # input: RRC r, count, L/R, A/L
         opcode = 25
         bin_str = process_binstr_count(opcode, con_value)
         return "RRC", binary2octal(bin_str, 6)
     elif op == "IN":
+        # input: IN r, devid
         opcode = 26
         bin_str = process_binstr_IO(opcode, con_value)
         return "IN", binary2octal(bin_str, 6)
     elif op == "OUT":
+        # input: OUT r, devid
         opcode = 27
         bin_str = process_binstr_IO(opcode, con_value)
         return "OUT", binary2octal(bin_str, 6)
     elif op == "CHK":
+        # input: CHK r, devid
         opcode = 28
         bin_str = process_binstr_IO(opcode, con_value)
         return "CHK", binary2octal(bin_str, 6)
     elif op == "FADD":
+        # input: FADD fr, x, address[,I]
         opcode = 29
         if len(con_value) == 3:
+            # if there isn't I value, append zero
             con_value.append(0)
         bin_str = process_binstr_register(opcode, con_value)
         return "FADD", binary2octal(bin_str, 6)
     elif op == "FSUB":
+        # input: FSUB fr, x, address[,I]
         opcode = 30
         if len(con_value) == 3:
+            # if there isn't I value, append zero
             con_value.append(0)
         bin_str = process_binstr_register(opcode, con_value)
         return "FSUB", binary2octal(bin_str, 6)
     elif op == "VADD":
+        # input: VADD fr, x, address[,I]
         opcode = 31
         if len(con_value) == 3:
+            # if there isn't I value, append zero
             con_value.append(0)
         bin_str = process_binstr_register(opcode, con_value)
         return "VADD", binary2octal(bin_str, 6)
     elif op == "VSUB":
+        # input: VSUB fr, x, address[,I]
         opcode = 32
         if len(con_value) == 3:
+            # if there isn't I value, append zero
             con_value.append(0)
         bin_str = process_binstr_register(opcode, con_value)
         return "VSUB", binary2octal(bin_str, 6)
     elif op == "CNVRT":
+        # input: CNVRT fr, x, address[,I]
         opcode = 33
         if len(con_value) == 3:
+            # if there isn't I value, append zero
             con_value.append(0)
         bin_str = process_binstr_register(opcode, con_value)
         return "CNVRT", binary2octal(bin_str, 6)
     elif op == "LDFR":
+        # input: LDFR fr, x, address[,I]
         opcode = 34
         if len(con_value) == 3:
+            # if there isn't I value, append zero
             con_value.append(0)
         bin_str = process_binstr_register(opcode, con_value)
         return "LDFR", binary2octal(bin_str, 6)
     elif op == "STFR":
+        # input: STFR fr, x, address[,I]
         opcode = 35
         if len(con_value) == 3:
+            # if there isn't I value, append zero
             con_value.append(0)
         bin_str = process_binstr_register(opcode, con_value)
         return "STFR", binary2octal(bin_str, 6)
     elif op == "SETCCE":
+        # input: SETCCE r
         opcode = 36
-        con_value = con_value + [0, 0, 0]
+        con_value = con_value + [0, 0, 0] # offset the ignored params
         bin_str = process_binstr_register(opcode, con_value)
         return "SETCCE", binary2octal(bin_str, 6)
     elif op == "TRAP":
+        # input: TRAP code. TRAP code use the address part in binary numbers.
         opcode = 37
-        con_value = [0, 0] + con_value + [0]
+        con_value = [0, 0] + con_value + [0] # offset the ignored params
         bin_str = process_binstr_register(opcode, con_value)
         return "SETCCE", binary2octal(bin_str, 6)
     
@@ -339,8 +411,8 @@ if __name__ == "__main__":
     LOC_pointer = 0
     f_out = open("output.txt", "w")
     filename = 'input.txt'
- 
     for line in fileinput.input(files=filename):
+        # read input file line by line.
         output_str, LOC_pointer = process(line, LOC_pointer)
         f_out.write(output_str + '\n')
     f_out.close()
